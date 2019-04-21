347cc964f6eeae3fdcf7bb90e5d30be7accd8f42
bhirschberger98 <bretthirschberger@gmail.com>
2019-04-21 02:10:10 -0400

    First Commit

---
+++ Final Project/src/Card.java
@@ -0,0 +1,15 @@
+import javafx.scene.image.Image;
+
+public class Card {
+	private int value;
+	private Image image;
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public Image getImage() {
+		return image;
+	}
+}
