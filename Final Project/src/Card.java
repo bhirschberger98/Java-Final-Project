fa9b07f03360d215df6a0b932a777c51b9926bae
bhirschberger98 <bretthirschberger@gmail.com>
2019-04-21 20:13:00 -0400

    Loads cards automatically

--- Final Project/src/Card.java
+++ Final Project/src/Card.java
@@ -3,13 +3,18 @@ import javafx.scene.image.Image;
 public class Card {
 	private int value;
 	private Image image;
+	public Card(Image image,int value) {
+		this.value=value;
+		this.image=image;
+	}
 	public int getValue() {
 		return value;
 	}
-	public void setValue(int value) {
-		this.value = value;
-	}
 	public Image getImage() {
 		return image;
+	}
+	@Override
+	public String toString() {
+		return "Value:"+value+ ", Image: "+image.impl_getUrl();
 	}
 }
--- Final Project/src/Controller.java
+++ Final Project/src/Controller.java
@@ -1,4 +1,44 @@
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 
 public class Controller {
-
+	private Card[] cards=new Card[52];
+    
+	@FXML
+    private Pane pane;
+	//loads in cards when program is loaded
+	{
+		File[] files = new File("PlayingCards/").listFiles();
+		for(int i=0;i<cards.length;i++) {
+			int value=0;
+			try {
+				value=Integer.parseInt(files[i].getName().substring(0, files[i].getName().indexOf("_")));
+			}
+			catch(NumberFormatException e) {
+				switch(files[i].getName().substring(0, files[i].getName().indexOf("_"))){
+				case "ace":
+					value=1;
+					break;
+				case "jack":
+					value=11;
+					break;
+				case "queen":
+					value=12;
+					break;
+				case "king":
+					value=13;
+					break;
+				}
+			}
+			cards[i]=new Card(new Image(files[i].toURI().toString()),value);
+		}
+		displayCards();
+	}
+	
+	public void displayCards() {
+		
+	}
 }
