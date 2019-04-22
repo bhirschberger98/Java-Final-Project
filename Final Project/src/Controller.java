f3676f9a129eb71fc285a8600139cd06115444c6
bhirschberger98 <bretthirschberger@gmail.com>
2019-04-21 21:13:59 -0400

    Loads cards when refresh is pressed

--- Final Project/src/Controller.java
+++ Final Project/src/Controller.java
@@ -1,16 +1,27 @@
 import java.io.File;
+import java.util.Random;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
 
 public class Controller {
 	private Card[] cards=new Card[52];
     
-	@FXML
-    private Pane pane;
+    @FXML
+    private ImageView card1=new ImageView();
+
+    @FXML
+    private ImageView card2=new ImageView();
+
+    @FXML
+    private ImageView card3=new ImageView();
+
+    @FXML
+    private ImageView card4=new ImageView();
+
 	//loads in cards when program is loaded
-	{
+	public Controller(){
 		File[] files = new File("PlayingCards/").listFiles();
 		for(int i=0;i<cards.length;i++) {
 			int value=0;
@@ -35,10 +46,12 @@ public class Controller {
 			}
 			cards[i]=new Card(new Image(files[i].toURI().toString()),value);
 		}
-		displayCards();
+
 	}
-	
 	public void displayCards() {
-		
+		card1.setImage(cards[new Random().nextInt(51)].getImage());
+		card2.setImage(cards[new Random().nextInt(51)].getImage());
+		card3.setImage(cards[new Random().nextInt(51)].getImage());
+		card4.setImage(cards[new Random().nextInt(51)].getImage());
 	}
 }
